b3b02981d5bc6517886e80d6a6dcebe57d0d7dda
WPJ008 <wawitephilemon@gmail.com>
2026-06-22 00:16:29 +0200

    requete sql model Event

---
+++ src/main/java/sql/bdd/EventManager.java
@@ -0,0 +1,158 @@
+package sql.bdd;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import model.Event;
+import model.User;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class EventManager {
+
+    static int id;
+
+    static Connection connection =  DatabaseConnection.getConnection();
+
+
+    //id_event	title_event,	description_event,	lieu_event,	date_event,	price_event,	place_total_event,	status_event,	organizer_id,	created_at,	updated_at
+    public static void registerEvent(Event event){
+
+        try {
+
+            String sql_request = "INSERT INTO events(title_event, description_event, lieu_event, date_event, price_event, place_total_event, status_event, organizer_id, created_at, updated_at) VALUES(?,?,?,?,?,?,?,?, NOW(), NOW())";
+
+            String request = "SELECT * FROM statuts WHERE name_status = 'PLANIFIE'";
+
+            PreparedStatement stm = connection.prepareStatement(request);
+            ResultSet result = stm.executeQuery();
+
+            //étape 5: extraire les donnéesQ
+            while(result.next()){
+                //Récupérer par nom de colonne
+                id = result.getInt("id_status");
+            }
+
+            PreparedStatement pstmt = connection.prepareStatement(sql_request);
+
+            pstmt.setString(1, event.getTitlEvent());
+            pstmt.setString(2, event.getDescriptionEvent());
+            pstmt.setString(3, event.getLieuEvent());
+            pstmt.setTimestamp(4, Timestamp.valueOf(event.getDatEvent()));
+            pstmt.setDouble(5, event.getPricEvent());
+            pstmt.setInt(6, event.getPlaceTotalEvent());
+            pstmt.setInt(7, id);
+            pstmt.setLong(8, event.getOrganizerId().getIdUser());
+
+            if (!pstmt.execute()) {
+                System.out.println("Event created with successfull !");
+            }
+
+        }catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+
+
+    }
+
+
+    public static Event getEvent(){
+
+
+        try {
+            PreparedStatement querySelect = connection.prepareStatement("SELECT * FROM events");
+
+           // querySelect.setString(1, email_user);
+
+            ResultSet result = querySelect.executeQuery();
+
+
+
+            while (result.next()) {
+
+
+                    String titre = result.getNString("title_event");
+                    String description = result.getNString("description_event");
+                    String lieu = result.getNString("lieu_event");
+                    Timestamp timestamp = result.getTimestamp("date_event");
+                    LocalDateTime datEvent = timestamp.toLocalDateTime();
+                    double  price = result.getDouble("price_event");
+                    int  placeTotalEvent = result.getInt("place_total_event");
+
+                    Event event = new Event();
+                    event.setTitlEvent(titre);
+                    event.setDescriptionEvent(description);
+                    event.setLieuEvent(lieu);
+                    event.setDatEvent(datEvent);
+                    event.setPricEvent(price);
+                    event.setPlaceTotalEvent(placeTotalEvent);
+
+
+                    // System.out.println("Prenom : " + user.getFirstName());
+                    // System.out.println("Nom : " + user.getLastName());
+                    // System.out.println("Email : " + user.getEmail());
+
+                    return event;
+
+            }
+           /* } else {
+                System.out.println("Email incorrect !");
+            }*/
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+        }
+
+
+        return null;
+    }
+
+
+    public static Event getEventByUser(User user){
+
+
+        try {
+            PreparedStatement querySelect = connection.prepareStatement("SELECT * FROM events WHERE organizer_id = ?");
+
+             querySelect.setLong(1, user.getIdUser());
+
+            ResultSet result = querySelect.executeQuery();
+
+
+
+            while (result.next()) {
+
+
+                String titre = result.getNString("title_event");
+                String description = result.getNString("description_event");
+                String lieu = result.getNString("lieu_event");
+                Timestamp timestamp = result.getTimestamp("date_event");
+                LocalDateTime datEvent = timestamp.toLocalDateTime();
+                double  price = result.getDouble("price_event");
+                int  placeTotalEvent = result.getInt("place_total_event");
+
+                Event event = new Event();
+                event.setTitlEvent(titre);
+                event.setDescriptionEvent(description);
+                event.setLieuEvent(lieu);
+                event.setDatEvent(datEvent);
+                event.setPricEvent(price);
+                event.setPlaceTotalEvent(placeTotalEvent);
+
+
+                return event;
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+        }
+
+
+        return null;
+    }
+}
